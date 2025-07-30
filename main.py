5368a6ba3575f6b03fa9a12457d9fce4a8f66f16
diego192599 <di.cramire@udeo.edu.gt>
2025-07-30 10:27:17 -0600

    creacion de menu interactivo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def MCD(n1,n2):
     if n1/n2==0:
         return 0
     else:
+        return MCD(n1%n2)
 def Repeticion(palabra,veces):
     if veces==0:
         return 0
@@ -28,4 +29,26 @@ while True:
     print("==Menu==")
     print("1. Minimo comun divisor")
     print("2. Repeticion de palabras")
-    print("3. Contar letras")
+    print("3. Contar Letras en cadena")
+    print("4. Convertir a binario")
+    print("5. Calcular digitos de un numero")
+    print("6. Salir")
+    opcion=int(input("Seleccione una opcion"))
+    if opcion==1:
+        print("---Minimo comun divisor---")
+        n1=int(input("Ingrese su primer numero (debe ser numero enteros positivos): "))
+        n2=int(input("Ingrese el segundo numero (debe ser numero entero positivo): "))
+    elif opcion==2:
+        print("---Repeticion de palabras---")
+        palabra=input("Ingrese la palabra que sea repetir: ")
+        veces=int(input("Ingrese la veces que se repetira la palabra: "))
+    elif opcion==3:
+        print("---Contar letras---")
+        cadena=input("Ingrese la cadena: ")
+        letra=input("Ingrese la letra: ")
+    elif opcion==4:
+        print("---Convertir a Binario---")
+        n=int(input("Ingrese un numero para convertir a binario: "))
+    elif opcion==5:
+        print("---Calcular digitos---")
+        n=int(input("Ingrese un numero (entero positivo): "))
