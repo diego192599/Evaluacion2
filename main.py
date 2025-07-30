da8049b336e01a1bd257dac9382aeafaac7b8d49
diego192599 <di.cramire@udeo.edu.gt>
2025-07-30 10:15:40 -0600

    creacion de metodos

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Menu")
+def MCD(n1,n2):
+    if n1/n2==0:
+        return 0
+    else:
+def Repeticion(palabra,veces):
+    if veces==0:
+        return 0
+    else:
+        return 1+Repeticion(palabra,veces)
+def contar_letras(cadena,letra):
+    if letra=="":
+        return 0
+    else:
+        return
+
+def conversion_Binario(n):
+    if n/2==0:
+        return 0
+    else:
+
+def numeros_digitos(n):
+    if n==0:
+        return 0
+    else:
+        return 1+numeros_digitos(n-1)
+
+while True:
+    print("==Menu==")
+    print("1. Minimo comun divisor")
+    print("2. Repeticion de palabras")
+    print("3. Contar letras")
